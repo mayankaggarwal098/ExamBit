d65bbc5df1c11f7123ffbc52580ab1c5044d504c
unknown <aggarwalmayank098@gmail.com>
2020-12-23 18:44:57 +0530

    testing

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV == "production") {
+//   app.use(express.static("client/build"));
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
