7e5c0bc7f27dfe1ed8eef21b381babd6f54688ee
unknown <aggarwalmayank098@gmail.com>
2020-12-23 19:04:24 +0530

    user model

--- index.js
+++ index.js
@@ -10,19 +10,11 @@ mongoose
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB..."));
 
-require("./models/user");
+// require("./models/user");
 
 app.use(express.json());
-app.use(require("./routes/auth"));
-app.use(require("./routes/post"));
-app.use(require("./routes/user"));
-
-// if (process.env.NODE_ENV == "production") {
-//   app.use(express.static("client/build"));
-//   const path = require("path");
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+// app.use(require("./routes/auth"));
+// app.use(require("./routes/post"));
+// app.use(require("./routes/user"));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
