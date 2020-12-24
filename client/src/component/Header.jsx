b86a8d07e071521dae2f5d1e875dee29a50b3fac
sagarsinghrawat <sagarrawatssr9647@gmail.com>
2020-12-24 17:31:54 +0530

    update header file

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-import { Nav, Navbar, Container} from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap'
-
+import { useDispatch, useSelector } from 'react-redux'
+import { Nav, Navbar, Container, NavDropdown} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { logout } from '../actions/userAction'
 const Header = () => {
+
+    const dispatch = useDispatch();
+
+    const { userInfo } = useSelector( state => state.userLogin )
+  
+    const logOutHandler = () => {
+      dispatch(logout());
+    }
+
     return (
         <header>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -17,13 +27,25 @@ const Header = () => {
                     <Nav.Link href="#pricing">Pricing</Nav.Link> */}
                 </Nav>
                 <Nav>
-                    <LinkContainer to="/login">
-                        <Nav.Link>Login</Nav.Link>
-                    </LinkContainer>
+                    { userInfo ? (
+                        <NavDropdown title={userInfo.name.toUpperCase()} id="username">
+                            <LinkContainer to='/profile'>
+                                <NavDropdown.Item>PROFILE</NavDropdown.Item>
+                            </LinkContainer>
+                            <NavDropdown.Item onClick={logOutHandler}>LOGOUT</NavDropdown.Item>
+                      </NavDropdown>
+                    ) : (
+                        <>
+                            <LinkContainer to="/login">
+                                <Nav.Link>Login</Nav.Link>
+                            </LinkContainer>
                 
-                    <LinkContainer to="/register">
-                        <Nav.Link>Sign Up</Nav.Link>
-                    </LinkContainer>
+                            <LinkContainer to="/register">
+                                <Nav.Link>Sign Up</Nav.Link>
+                            </LinkContainer>
+                        </>
+                    )}
+                    
                 </Nav>
                 
             </Navbar.Collapse>
