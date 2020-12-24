12b222a8b3c87c9124de4f075b975335a0cb56e1
sagarsinghrawat <sagarrawatssr9647@gmail.com>
2020-12-24 17:30:12 +0530

    update register screen

--- client/src/screen/Register.jsx
+++ client/src/screen/Register.jsx
@@ -1,67 +1,106 @@
-import React from 'react'
+import React, {useState, useEffect }from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
+import { userRegister } from '../actions/userAction';
+import { toast } from 'react-toastify';
 
-const Register = () => {
-    const submitHandler = () => {
+const Register = ({history}) => {
 
+    const [ name, setName ] = useState('');
+    const[ email, setEmail ] = useState('');
+    const[ password, setPassword ] = useState('');
+    const[ confirmPassword, setConfirmPassword ] = useState('');
+    const[ category, setCategory ] = useState('');
+
+    const { loading, success, error } = useSelector( state => state.register )
+
+    const dispatch = useDispatch();
+
+    useEffect( () => {
+        if( success ){
+            history.push('/login');
+        }
+    }, [history, success]);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if( password !== confirmPassword ) {
+            toast.error('Invalid Input')
+        } else{
+            dispatch( userRegister( name, email, password, category ));
+        }
     }
 
     return (
         <Container>
             <Row className="justify-content-md-center my-5">
                 <Col sx={12} md={6}>
+                <h2>SIGN UP</h2>
                 <Form onSubmit={submitHandler}>
-                <Form.Group controlId="name">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                        type="name"
-                        placeholder="Enter name"
-                    ></Form.Control>
-                </Form.Group>
-                <Form.Group controlId="email">
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control
-                        type="email"
-                        placeholder="Enter Email Address"
-                    ></Form.Control>
-                </Form.Group>
+                
+                    <Form.Group controlId="name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            type="name"
+                            value={name}
+                            placeholder="Enter name"
+                            onChange={ e => setName(e.target.value)}
+                        ></Form.Control>
+                    </Form.Group>
 
-                <Form.Group controlId="category">
-                    <Form.Label>Category</Form.Label>
-                    <Form.Control
-                        as="select"
-                        >
-                            <option>Supervisor</option>
-                            <option>Student</option>
-                    </Form.Control>
-                </Form.Group>
+                    <Form.Group controlId="email">
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control
+                            type="email"
+                            value={email}
+                            placeholder="Enter Email Address"
+                            onChange={ e => setEmail(e.target.value)}
+                        ></Form.Control>
+                    </Form.Group>
 
-                <Form.Group controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Enter Password"
-                    ></Form.Control>
-                </Form.Group>
-                <Form.Group controlId="confirmPassword">
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control
-                        type="confirmPassword"
-                        placeholder="Confirm Password"
-                    ></Form.Control>
-                </Form.Group>
+                    <Form.Group controlId="category">
+                        <Form.Label>Category</Form.Label>
+                        <Form.Control
+                            as="select"
+                            value={category}
+                            onChange={ e => setCategory(e.target.value)}
+                            >
+                                <option value=''>Select Category</option>
+                                <option value='Supervisor'>Supervisor</option>
+                                <option value='Student'>Student</option>
+                        </Form.Control>
+                    </Form.Group>
 
-                <Button type="submit">
-                    Submit
-                </Button>
+                    <Form.Group controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            value={password}
+                            placeholder="Enter Password"
+                            onChange={ e => setPassword(e.target.value) }
+                        ></Form.Control>
+                    </Form.Group>
 
-                <Row className="py-3 px-3">
+                    <Form.Group controlId="confirmPassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            value={confirmPassword}
+                            placeholder="Confirm Password"
+                            onChange={ e => setConfirmPassword(e.target.value)}
+                        ></Form.Control>
+                    </Form.Group>
+
+                    <Button type="submit" variant="primary">
+                        Submit
+                    </Button>
+
+                    <Row className="py-3 px-3">
                         ALREADY HAVE AN ACCOUNT ? &nbsp;&nbsp; <Link to='/login'>SIGN IN</Link>
-                        
-                </Row> 
-            </Form>
 
+                    </Row> 
+            </Form>
             </Col>
             </Row>
         </Container>
