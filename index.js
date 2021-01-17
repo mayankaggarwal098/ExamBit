46f983a0d5660153de4e496c79a1060ec661de48
unknown <aggarwalmayank098@gmail.com>
2021-01-17 19:23:43 +0530

    heroku code added

--- index.js
+++ index.js
@@ -29,4 +29,12 @@ app.use("/api/supervisor", admin);
 app.use("/api/snapshot", snapshots);
 //createadmin();
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
